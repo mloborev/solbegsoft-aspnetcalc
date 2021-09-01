a31c61d4ef768557d29f4aad41157fdbdecf9d12
mloborev <39646455+mloborev@users.noreply.github.com>
2021-09-01 22:44:09 +0300

    Add files via upload

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using AspNetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetApp.Controllers
+{
+    [Route("/")]
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(Calculator c, string calculate)
+        {
+            if (calculate == "Addition")
+            {
+                c.Result = c.Num1 + c.Num2;
+            }
+            else if (calculate == "Subtraction")
+            {
+                c.Result = c.Num1 - c.Num2;
+            }
+            else if (calculate == "Multiplication")
+            {
+                c.Result = c.Num1 * c.Num2;
+            }
+            else if (calculate == "Division")
+            {
+                if (c.Num2 != 0)
+                    c.Result = c.Num1 / c.Num2;
+                else
+                    ViewBag.Error = "На ноль делить нельзя";
+            }
+            else if (calculate == "Clear")
+            {
+                c.Num1 = 0;
+                c.Num2 = 0;
+                c.Result = 0;
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetApp.Models
+{
+    public class Calculator
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public double Result { get; set; }
+    }
+}
